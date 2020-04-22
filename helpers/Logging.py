dac1c41ee43629f697c8311012a815c21e93d3a0
Zane Gittins <Zane.Gittins@meissner.com>
2020-04-22 15:04:17 -0700

    Minor change to logging format.

--- helpers/Logging.py
+++ helpers/Logging.py
@@ -4,7 +4,7 @@ def log(message, logname):
     log_file_name = logname + ".txt"
     log_file = open(log_file_name,"a+")
     current_time = str(time.strftime('%Y-%m-%d %H:%M:%S'))
-    to_log = ("[" + logname + "][" + current_time + "  ]: " + message)
+    to_log = ("[" + logname + "][" + current_time + "]: " + message)
     print(to_log)
     if to_log[-1:] != "\n":
         to_log += "\n"
